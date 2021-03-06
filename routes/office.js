b970495f2b4b5e8595853f32db126ca512377121
Raistlin TAO <DemonVK@Gmail.com>
2021-03-06 21:38:50 +0800

    Office List/Adding

--- routes/office.js
+++ routes/office.js
@@ -1,8 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const db = require('../modules/db');
 
 router.get('/', function (req, res, next) {
-    res.render('office', {title: 'Express'});
+    if (req.session.loggedin && req.session.userID) {
+        db.query('SELECT * FROM office WHERE userID = ' + req.session.userID, function (error, results, fields) {
+            if (error) {
+                res.redirect('/');
+            }
+            let username = req.session.username;
+            res.render('office', {userName: username, offices: results});
+        });
+    } else {
+        res.redirect('/');
+    }
+});
+
+/* POST Office Insertion . */
+router.post('/add', function (request, response) {
+    if (request.session.loggedin) {
+        let officeName = request.body.officeName;
+        if (officeName) {
+            let query = "INSERT INTO `office` (officeName, officeStatus, userID) VALUES ('" +
+                officeName + "', '1', '" + request.session.userID + "')";
+            db.query(query, (err, result) => {
+                response.redirect('/office');
+            });
+        } else {
+            response.redirect('/office');
+        }
+    } else {
+        response.redirect('/');
+    }
 });
 
 module.exports = router;
