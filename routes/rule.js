ffad31eca837bce7c63f8dcb5bdb4f1221adc5b6
Raistlin TAO <DemonVK@Gmail.com>
2021-03-08 22:50:15 +0800

    Support Area Selection for Intrusion Detection

--- routes/rule.js
+++ routes/rule.js
@@ -3,40 +3,55 @@ const router = express.Router();
 const db = require('../modules/db');
 
 router.get('/', function (req, res, next) {
-    res.render('rule');
+    if (req.session.loggedin) {
+        db.query('SELECT * FROM rule JOIN camera ON rule.cameraID = camera.cameraID WHERE rule.userID = ' + req.session.userID, function (error, results, fields) {
+            if (error) {
+                res.redirect('/');
+            }
+            db.query('SELECT * FROM camera JOIN office ON camera.officeID = office.officeID WHERE camera.userID = ' + req.session.userID, function (error, cameraResults, fields) {
+                if (error) {
+                    res.redirect('/');
+                }
+                let username = req.session.username;
+                res.render('rule', {userName: username, rules: results, cameras: cameraResults});
+            });
+        });
+    } else {
+        res.redirect('/user/login');
+    }
 });
 
 /* POST Rule Insertion . */
-router.post('/add', function (request, response) {
-    if (request.session.loggedin) {
-        let officeName = request.body.officeName;
+router.post('/add', function (req, res) {
+    if (req.session.loggedin) {
+        let officeName = req.body.officeName;
         if (officeName) {
             let query = "INSERT INTO `office` (officeName, officeStatus, userID) VALUES ('" +
                 officeName + "', '1', '" + request.session.userID + "')";
             db.query(query, (err, result) => {
-                response.redirect('/office');
+                res.redirect('/rule');
             });
         } else {
-            response.redirect('/office');
+            res.redirect('/rule');
         }
     } else {
-        response.redirect('/');
+        res.redirect('/user/login');
     }
 });
 
 /* DELETE RULE */
-router.get('/del/:ruleID', function (request, response, next) {
-    if (request.session.loggedin) {
-        let ruleID = request.params.ruleID;
+router.get('/del/:ruleID', function (req, res, next) {
+    if (req.session.loggedin) {
+        let ruleID = req.params.ruleID;
         if (ruleID) {
             let query = "DELETE FROM `rule` WHERE ruleID = '" + ruleID + "'";
             db.query(query, (err, result) => {
                 //response.send(err.toString());
-                response.redirect('/rule');
+                res.redirect('/rule');
             });
         }
     } else {
-        response.redirect('/');
+        res.redirect('/user/login');
     }
 });
 
