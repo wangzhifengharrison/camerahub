50a3a70025bd86e2e149710f6ef289973bd32b99
Raistlin TAO <DemonVK@Gmail.com>
2021-03-10 03:09:41 +0800

    Accept Alert Attachment POST Request

--- app.js
+++ app.js
@@ -34,11 +34,10 @@ app.use(session({
 }))
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({limit: '25mb'}));
+app.use(express.urlencoded({extended: false, limit: '25mb'}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/camera', cameraRouter);
 app.use('/office', officeRouter);
--- routes/alert.js
+++ routes/alert.js
@@ -3,26 +3,40 @@ const router = express.Router();
 const db = require('../modules/db');
 const fs = require("fs");
 
+router.get('/', function (req, res, next) {
+    db.query('SELECT userID FROM camera WHERE cameraID = ' + '1', function (error, results, fields) {
+        let resultArray = results[0];
+        res.send('Result: ' + results[0]['userID']);
+    });
+});
+
 /* POST Alert Insertion . */
 router.post('/add', function (req, res) {
     let alertType = req.body.alertType;
     let alertMessage = req.body.alertMessage;
     let alertTimestamp = req.body.alertTimestamp;
     let cameraID = req.body.cameraID;
+    console.log('alertMessage: ' + alertMessage);
     //Convert Base64 Image to file and store the filename in the database
-    let alertAttachedPicture = new Buffer(req.body.alertAttachedPicture, 'base64');
-    fs.writeFileSync("public/alert/" + alertTimestamp + ".png", alertAttachedPicture);
+    //let alertAttachedPicture = new Buffer(req.body.alertAttachedPicture, 'base64');
+    let alertAttachedPicture = req.body.alertAttachedPicture.replace(/^data:image\/png;base64,/, "");
+    fs.writeFile("public/alert/" + alertTimestamp + ".png", alertAttachedPicture, 'base64', function (err) {
+        console.log(err);
+    });
+    //fs.writeFileSync("public/alert/" + alertTimestamp + ".png", alertAttachedPicture);
     //Get the userID from cameraID
-    let userID;
-    if (alertType && alertMessage && alertTimestamp) {
-        let query = "INSERT INTO `alert` (alertMessage, alertType, alertTime, alertAttachment, userID, cameraID) VALUES ('" +
-            alertMessage + "', '" + alertType + "', '" + alertTimestamp + "', '" + alertTimestamp + ".png" + "', '" + userID + "', '" + cameraID + "')";
-        db.query(query, (err, result) => {
-            res.redirect('/office');
-        });
-    } else {
-        res.redirect('/');
-    }
+    db.query('SELECT userID FROM camera WHERE cameraID = ' + cameraID, function (error, results, fields) {
+        let userID = results[0]['userID'];
+        if (alertType && alertMessage && alertTimestamp) {
+            let query = "INSERT INTO `alert` (alertMessage, alertType, alertTime, alertAttachment, userID, cameraID) VALUES ('" +
+                alertMessage + "', '" + alertType + "', '" + alertTimestamp + "', '" + alertTimestamp + ".png" + "', '" + userID + "', '" + cameraID + "')";
+            db.query(query, (err, result) => {
+                res.send('OK');
+            });
+        } else {
+            res.send('ERROR');
+        }
+    });
 });
 
 /* DELETE Alert */
