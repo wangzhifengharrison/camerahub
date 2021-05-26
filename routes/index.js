fd27df54f6cf6e1ba3e0ad61e7ba381b0cb925d6
Raistlin TAO <DemonVK@Gmail.com>
2021-05-26 17:53:51 +0800

    User Interface Based on Customer PPT
    User Interface Based on Customer PPT
    ---- Overall
    * Remove Navibar
    * Remove Customer User Avatar
    * Add Circled Text for User Avatar
    * Relocate the User Info to buttom left
    * Add Company Logo on top left on Sidebar
    * Change golbal CSS primary color, highlight color
    * Add H3 page tilte in content part
    * Move the logout function at sidebar buttom left
    
    ---- Dashboard PAGE
    * Add Alert List with search function
    * Remove useless demo content
    * Change the notification trigger event in order to reflect the UI change
    
    ---- Office PAGE
    * Change Office to Location and related string value
    
    ---- Camera PAGE
    * Add Deactivated function for camera
    * Put search function in list
    
    ---- Alert PAGE
    TBA

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,15 @@ const db = require('../modules/db');
 /* GET home page. */
 router.get('/', function (req, res, next) {
     if (req.session.loggedin) {
-        res.locals.userID = req.session.userID;
-        res.locals.username = req.session.username;
-        res.render('main');
+        db.query('SELECT * FROM alert JOIN camera ON alert.cameraID = camera.cameraID WHERE alert.userID = ' + req.session.userID + ' ORDER BY alert.alertID DESC', function (error, results, fields) {
+            if (error) {
+                res.redirect('/');
+            }
+            res.locals.userID = req.session.userID;
+            res.locals.username = req.session.username;
+            res.render('main',{userName: res.locals.username, alerts: results});
+        });
+
     } else {
         res.redirect('/user/login');
     }
